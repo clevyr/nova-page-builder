67dd34dc15955fc4d9d45185be395f09e4b17442
Gabe Cook <gabe565@gmail.com>
2021-09-01 10:26:54 -0500

    Fix autoload dump failures due to menu generation

--- src/NovaPageBuilder.php
+++ src/NovaPageBuilder.php
@@ -2,11 +2,19 @@
 
 namespace Clevyr\NovaPageBuilder;
 
+use Illuminate\Support\Facades\Schema;
 use Inertia\Inertia;
 use Clevyr\NovaPageBuilder\Models\Page;
 
 class NovaPageBuilder
 {
+    public function __construct() {
+        // Share menu automatically if available
+        if (Schema::hasTable(config('nova-menu.menus_table_name'))) {
+            Inertia::share("navigations", nova_get_menus());
+        }
+    }
+
     public static function catchAll() {
         $page = config('nova-page-builder.model', Page::class)::where('slug', request()->path())
             ->where('is_published', 1)
