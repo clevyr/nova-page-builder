29c7a671de5460fae460d1c6c45d185cba65ea46
Anthony Protano <anthony@clevyr.com>
2025-03-31 11:33:13 -0400

    Feat: Add User and Seeder

--- workbench/database/seeders/DatabaseSeeder.php
+++ workbench/database/seeders/DatabaseSeeder.php
@@ -5,11 +5,16 @@ declare(strict_types=1);
 namespace Workbench\Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Workbench\App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
     public function run(): void
     {
+        User::factory()->create([
+            'test@example.com'
+        ]);
 
+        User::factory()->count(2)->create();
     }
 }
