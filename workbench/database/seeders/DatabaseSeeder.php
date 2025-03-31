fa1b84790880f87efcd1afc18b2844103d151a19
Anthony Protano <anthony@clevyr.com>
2025-03-31 09:45:55 -0400

    Feat: Add Workbecnh

---
+++ workbench/database/seeders/DatabaseSeeder.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Workbench\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class DatabaseSeeder extends Seeder
+{
+    public function run(): void
+    {
+
+    }
+}
