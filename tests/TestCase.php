585e364d04414f87f3ee23113ff7e2d74ee7f4ae
Anthony Protano <anthony@clevyr.com>
2025-03-31 15:12:59 -0400

    Test: Init Pest

---
+++ tests/TestCase.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Clevyr\NovaPageBuilder\Tests;
+
+use Orchestra\Testbench\Concerns\WithWorkbench;
+
+abstract class TestCase extends \Orchestra\Testbench\TestCase
+{
+    use WithWorkbench;
+}
