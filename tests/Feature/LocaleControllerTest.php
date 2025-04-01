182e468b98d963fffece3eaf7ad6b3e77faf3523
Anthony Protano <anthony@clevyr.com>
2025-04-01 13:50:52 -0400

    CI: Set app key

--- tests/Feature/LocaleControllerTest.php
+++ tests/Feature/LocaleControllerTest.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use function Pest\Laravel\withHeaders;
 
 it('sets locale sets session and redirects', function (string $referer) {
-    $locale = 'en';
+    $locale = array_key_first(config('nova-page-builder.locales'));
 
     withHeaders(['Referer' => $referer])->get(route('set-locale', $locale))
         ->assertSessionHas('locale', $locale)
