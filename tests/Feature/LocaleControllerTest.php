0d98c6edcde634c077125403a9e1af60612f6661
Anthony Protano <anthony@clevyr.com>
2025-04-01 13:28:00 -0400

    CI: Remove array

--- tests/Feature/LocaleControllerTest.php
+++ tests/Feature/LocaleControllerTest.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 use function Pest\Laravel\withHeaders;
 
 it('sets locale sets session and redirects', function (string $referer) {
-    $locale = array_key_first(config('nova-page-builder.locales'));
+    $locale = 'en';
 
     withHeaders(['Referer' => $referer])->get(route('set-locale', $locale))
         ->assertSessionHas('locale', $locale)
