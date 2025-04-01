b54abfe27bed1ba0fa1babe7211bb11a636340dd
Anthony Protano <anthony@clevyr.com>
2025-04-01 15:04:10 -0400

    CI: Try with providers in testcase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,9 +3,19 @@
 namespace Clevyr\NovaPageBuilder\Tests;
 
 use Orchestra\Testbench\Concerns\WithWorkbench;
-use Outl1ne\MenuBuilder\MenuBuilderServiceProvider;
 
 abstract class TestCase extends \Orchestra\Testbench\TestCase
 {
     use WithWorkbench;
+
+    protected function getPackageProviders($app): array
+    {
+        return [
+            \Laravel\Nova\NovaServiceProvider::class,
+            \Laravel\Nova\NovaCoreServiceProvider::class,
+            \Workbench\App\Providers\NovaServiceProvider::class,
+            \Outl1ne\MenuBuilder\MenuBuilderServiceProvider::class,
+            \Clevyr\NovaPageBuilder\NovaPageBuilderServiceProvider::class,
+        ];
+    }
 }
